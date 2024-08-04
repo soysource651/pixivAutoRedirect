2dcfce1b8f9fcaaab1721f29ad74c611f3f7a1de
soysource651 <115977841+soysource651@users.noreply.github.com>
2024-08-04 22:01:44 +0900

    Update pixivAutoRedirect.user.js

--- pixivAutoRedirect.user.js
+++ pixivAutoRedirect.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         pixivAutoRedirect
-// @namespace    http://tampermonkey.net/
 // @version      1.0.0
-// @description  try to take over the world!
-// @author       You
+// @homepageURL https://github.com/soysource651/pixivAutoRedirect
+// @updateURL   https://github.com/soysource651/pixivAutoRedirect/raw/main/pixivAutoRedirect.user.js
+// @downloadURL https://github.com/soysource651/pixivAutoRedirect/raw/main/pixivAutoRedirect.user.js
+// @description  test
+// @author       soysource651
 // @match        https://www.pixiv.net/jump.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pixiv.net
 // @grant        none
