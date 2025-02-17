004809e050afc6d184aeb1a89b562a122821e937
soysource651 <115977841+soysource651@users.noreply.github.com>
2025-02-17 18:52:46 +0900

    Update pixivAutoRedirect.user.js

--- pixivAutoRedirect.user.js
+++ pixivAutoRedirect.user.js
@@ -1,17 +1,26 @@
 // ==UserScript==
 // @name         pixivAutoRedirect
-// @version      1.0.1
+// @version      1.1.0
 // @homepageURL https://github.com/soysource651/pixivAutoRedirect
 // @updateURL   https://github.com/soysource651/pixivAutoRedirect/raw/main/pixivAutoRedirect.user.js
 // @downloadURL https://github.com/soysource651/pixivAutoRedirect/raw/main/pixivAutoRedirect.user.js
 // @description  test
 // @author       soysource651
 // @match        https://www.pixiv.net/jump.php*
+// @match        https://nijie.info/jump.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pixiv.net
 // @grant        none
 // ==/UserScript==
 
 (function() {
-   const Link = unescape (window.location.href.substr(window.location.href.indexOf('=')+1));
-   window.location.href = Link;
+   var URL = window.location.href;
+
+   if ( URL.indexOf('https://www.pixiv.net/jump.php')===0) {
+       const Link = unescape (window.location.href.substr(window.location.href.indexOf('=')+1));
+       window.location.href = Link;
+   }
+   else if (URL.indexOf('https://nijie.info/jump.php')===0) {
+       const Link = document.querySelector("p.bold.size24").querySelector('a').getAttribute('href');
+       window.location.href = Link;
+   }
 })();
