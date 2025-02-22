db4e2ab36386d611e59a8fbef47391d55dffc974
soysource651 <115977841+soysource651@users.noreply.github.com>
2025-02-22 10:44:31 +0900

    Update pixivAutoRedirect.user.js

--- pixivAutoRedirect.user.js
+++ pixivAutoRedirect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         pixivAutoRedirect
-// @version      1.1.0
+// @version      1.1.1
 // @homepageURL https://github.com/soysource651/pixivAutoRedirect
 // @updateURL   https://github.com/soysource651/pixivAutoRedirect/raw/main/pixivAutoRedirect.user.js
 // @downloadURL https://github.com/soysource651/pixivAutoRedirect/raw/main/pixivAutoRedirect.user.js
